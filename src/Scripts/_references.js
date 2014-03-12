95557f2fc3cbc0dd3c6b9ab130776bde36bf094a
John Bledsoe <jmbledsoe@gmail.com>
2014-03-12 19:24:30 -0400

    Added source solution.

---
+++ src/Scripts/_references.js
@@ -0,0 +1,6 @@
+﻿/// <autosync enabled="false" />
+/// <reference path="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular.js" />
+/// <reference path="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular-animate.js" />
+/// <reference path="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.14/angular-route.js" />
+/// <reference path="_intellisense.js" />
+/// <reference path="_intellisense-tests.js" />
