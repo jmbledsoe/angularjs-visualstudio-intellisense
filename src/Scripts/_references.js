2be2eca6573cf515cad88cd2b8c7a67eb85b96ee
John Bledsoe <jmbledsoe@gmail.com>
2014-10-24 14:34:18 -0400

    Remove test files.

--- src/Scripts/_references.js
+++ src/Scripts/_references.js
@@ -2,7 +2,4 @@
 /// <reference path="angular-references.js" />
 /// <reference path="_intellisense.js" />
 /// <reference path="_intellisense-tests.js" />
-/// <reference path="two-services-one-module.js" />
-/// <reference path="common.js" />
-/// <reference path="logger.js" />
 
--- src/Scripts/logger.js
+++
@@ -1,7 +0,0 @@
-﻿(function () {
-    'use strict';
-
-    var commonModule = angular.module('common');
-
-    commonModule.factory('logger', function (common) { return { log: true } });
-})();
