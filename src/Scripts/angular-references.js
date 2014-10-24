2eac22f4a8c7b03f1cb89fa89b187e466f6bca85
John Bledsoe <jmbledsoe@gmail.com>
2014-10-24 13:47:12 -0400

    Moved AngularJS references to separate file for auto-updating of references.

---
+++ src/Scripts/angular-references.js
@@ -0,0 +1,3 @@
+﻿/// <reference path="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular.js" />
+/// <reference path="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular-animate.js" />
+/// <reference path="https://ajax.googleapis.com/ajax/libs/angularjs/1.2.26/angular-route.js" />
