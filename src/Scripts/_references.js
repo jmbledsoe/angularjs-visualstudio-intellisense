9a93faebf67a800cfdf658b88784e10d55bc522c
John Bledsoe <jmbledsoe@gmail.com>
2014-11-15 16:07:23 -0500

    Prevent auto-sync of _references.js

--- src/Scripts/_references.js
+++ src/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <autosync enabled="true" />
+﻿/// <autosync enabled="false" />
 /// <reference path="angular.js" />
 /// <reference path="angular-animate.js" />
 /// <reference path="angular-route.js" />
