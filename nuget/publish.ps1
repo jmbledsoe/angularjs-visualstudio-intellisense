bed3f3c2c3d372bb2a61f43a9e327e3db6ddca70
Shawn Cicoria <shawn@cicoria.com>
2015-02-27 11:33:36 -0500

    adding nuget building stuff

---
+++ nuget/publish.ps1
@@ -0,0 +1,17 @@
+﻿
+$apiKey = Read-Host -Prompt "What is your API Key?"
+
+wget http://nuget.org/nuget.exe -OutFile nuget.exe -ErrorAction Continue
+
+
+if (Test-Path '.\nuget.exe'){
+	$pubCmd = {.\nuget.exe push .\AngularJS.Intellisense.1.0.0.nupkg -ApiKey $apiKey}
+	Invoke-Command $pubCmd
+}
+else {
+	Write-Error "Nuget.exe not found"
+}
+
+
+
+
